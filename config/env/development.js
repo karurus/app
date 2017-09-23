2123d32c5177679b125e8be9a6bac18274ecc5d6
wuttipichai karunarat <wu_tipichai555@hotmail.com>
2017-09-23 11:02:31 +0700

    init commit

---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+// create new file ./config/env/development.js (2)
+// and ./config/env/production.js (3)
+module.exports = {
+    mongoUri: 'mongodb://localhost/dbapp',
+    // or use 
+    // mongoUri: 'mongodb://admin:1234@localhost:27017/dbapp',
+    debug: true,
+    sessionSecret: 'dev_secret_key'
+}
