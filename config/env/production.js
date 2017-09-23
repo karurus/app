74281b0b567e85b469d3624ac87b79c4f60407c4
wuttipichai karunarat <wu_tipichai555@hotmail.com>
2017-09-23 12:22:29 +0700

    update

---
+++ config/env/production.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoUri: 'mongodb://localhost/dbapp',
+    // or use 
+    // mongoUri: 'mongodb://admin:1234@localhost:27017/dbapp',
+    debug: true,
+    sessionSecret: 'dev_secret_key'
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from './config/express';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.PORT = process.env.PORT || 3000;
 
+var db = mongoose();
 var app = express();
 
 app.listen(process.env.PORT, () => {
