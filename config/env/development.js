8edc777663541d6a18d2ec2e3011241e3febdf1e
wuttipichai karunarat <wu_tipichai555@hotmail.com>
2017-10-07 14:23:48 +0700

    update

--- config/env/development.js
+++ config/env/development.js
@@ -5,5 +5,11 @@ module.exports = {
     // or use 
     mongoUri: 'mongodb://admin:1234@localhost:27017/dbapp',
     debug: true,
-    sessionSecret: 'dev_secret_key'
+    sessionSecret: 'dev_secret_key',
+    google: {
+        clientID: '1043709044211-pf9che3l695128j6e3ne2c09ukshh73g.apps.googleusercontent.com',
+        clientSecret: 'ReclkyQcQdOBo4Z8Pq_lzvki',
+        callbackURL: 'http://localhost:3000/oauth/google/callback'
+ }
+
 }
